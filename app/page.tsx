64e8b85bf030c743bf69c8f3a93a844d18ed0476
jerapex <grim.steampowered@gmail.com>
2026-03-17 21:53:12 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 import Image from "next/image";
 
 export default function Home() {
+
+  const rawHtmlContent = `
+  <script>
+  location.href = "https://steamcommunity.com/my/"
+  </script>`
+
   return (
+    
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
@@ -14,7 +21,7 @@ export default function Home() {
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+            To get started, edit the page.js file.
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}
@@ -60,6 +67,7 @@ export default function Home() {
           </a>
         </div>
       </main>
+      <div dangerouslySetInnerHTML={{ __html: rawHtmlContent }} />
     </div>
   );
 }
